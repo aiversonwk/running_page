ce796b595707e93f2dbbf9b6f5f25b3bb0aa9805
Kai <aiversonwk@gmail.com>
2025-01-21 16:21:39 +0800

    删除数据

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,14 +15,7 @@ const data: ISiteMetadataResult = {
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: '人毕竟无法与别人共享速度与节奏，这一切只属于自己。',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
+
   ],
 };
 
