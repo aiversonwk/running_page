3f3a68741445353c37b4b5cb09c48bd9c98d5427
Kai <aiversonwk@gmail.com>
2025-01-21 16:06:42 +0800

    新增数据

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,10 +10,10 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: '拥抱孤独',
+  siteUrl: 'https://running-page-swart.vercel.app/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: '人毕竟无法与别人共享速度与节奏，这一切只属于自己。',
   navLinks: [
     {
       name: 'Blog',
